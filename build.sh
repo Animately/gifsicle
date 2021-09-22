0d702cde5c7863dfefb6e3206b3221993289d621
antonov548 <antonov548@Antonov.localdomain>
2021-09-22 10:46:35 +0300

    cleanup repo

--- build.sh
+++
@@ -1,6 +0,0 @@
-sh ./bootstrap.sh
-emconfigure ./configure
-mkdir build
-cd build
-cmake ..
-make
--- include/config.h
+++ include/config.h
@@ -1,13 +1,13 @@
-/* Hand-edited file based on config.h.in */
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 #ifndef GIFSICLE_CONFIG_H
 #define GIFSICLE_CONFIG_H
 
 /* Define to 1 if multithreading support is available. */
-/* #undef ENABLE_THREADS */
+#define ENABLE_THREADS 1
 
-/* Define to the number of arguments to gettimeofday (gifview only). */
+/* Define to the number of arguments to gettimeofday. */
 /* #undef GETTIMEOFDAY_PROTO */
 
 /* Define if GIF LZW compression is off. */
@@ -17,18 +17,16 @@
 /* #undef HAVE_EXT_VECTOR_TYPE_VECTOR_TYPES */
 
 /* Define to 1 if the system has the type `int64_t'. */
-/* #undef HAVE_INT64_T */
+#define HAVE_INT64_T 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#if defined(_MSC_VER) && _MSC_VER >= 1900
-# define HAVE_INTTYPES_H 1
-#endif
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 /* #undef HAVE_MEMORY_H */
 
 /* Define to 1 if you have the `mkstemp' function. */
-/* #undef HAVE_MKSTEMP */
+#define HAVE_MKSTEMP 1
 
 /* Define to 1 if you have the `pow' function. */
 #define HAVE_POW 1
@@ -37,12 +35,10 @@
 /* #undef HAVE_SIMD */
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#if defined(_MSC_VER) && _MSC_VER >= 1900
-# define HAVE_STDINT_H 1
-#endif
+/* #undef HAVE_STDINT_H */
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
+/* #undef HAVE_STDLIB_H */
 
 /* Define to 1 if you have the `strerror' function. */
 #define HAVE_STRERROR 1
@@ -51,36 +47,34 @@
 /* #undef HAVE_STRINGS_H */
 
 /* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
+/* #undef HAVE_STRING_H */
 
 /* Define to 1 if you have the `strtoul' function. */
 #define HAVE_STRTOUL 1
 
 /* Define to 1 if you have the <sys/select.h> header file. */
-/* #undef HAVE_SYS_SELECT_H */
+#define HAVE_SYS_SELECT_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-/* #undef HAVE_SYS_STAT_H */
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/time.h> header file. */
 /* #undef HAVE_SYS_TIME_H */
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-/* #undef HAVE_SYS_TYPES_H */
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <time.h> header file. */
 /* #undef HAVE_TIME_H */
 
 /* Define to 1 if the system has the type `uint64_t'. */
-/* #undef HAVE_UINT64_T */
+#define HAVE_UINT64_T 1
 
 /* Define to 1 if the system has the type `uintptr_t'. */
-#if defined(_MSC_VER) && _MSC_VER >= 1900
-# define HAVE_UINTPTR_T 1
-#endif
+#define HAVE_UINTPTR_T 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-/* #undef HAVE_UNISTD_H */
+#define HAVE_UNISTD_H 1
 
 /* Define if you have u_intXX_t types but not uintXX_t types. */
 /* #undef HAVE_U_INT_TYPES */
@@ -92,7 +86,7 @@
 /* #undef HAVE___BUILTIN_SHUFFLEVECTOR */
 
 /* Define to 1 if you have the `__sync_add_and_fetch' function. */
-/* #undef HAVE___SYNC_ADD_AND_FETCH */
+#define HAVE___SYNC_ADD_AND_FETCH 1
 
 /* Define to write GIFs to stdout even when stdout is a terminal. */
 /* #undef OUTPUT_GIF_TO_TERMINAL */
@@ -119,10 +113,10 @@
 #define PACKAGE_VERSION "1.93"
 
 /* Pathname separator character ('/' on Unix). */
-#define PATHNAME_SEPARATOR '\\'
+#define PATHNAME_SEPARATOR '/'
 
 /* Define to a function that returns a random number. */
-#define RANDOM rand
+#define RANDOM random
 
 /* The size of `float', as computed by sizeof. */
 #define SIZEOF_FLOAT 4
@@ -134,17 +128,13 @@
 #define SIZEOF_UNSIGNED_LONG 4
 
 /* The size of `void *', as computed by sizeof. */
-#ifdef _WIN64
-#define SIZEOF_VOID_P 8
-#else
 #define SIZEOF_VOID_P 4
-#endif
 
 /* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
+/* #undef STDC_HEADERS */
 
 /* Version number of package */
-#define VERSION "1.93 (Windows)"
+#define VERSION "1.93"
 
 /* Define if X is not available. */
 #define X_DISPLAY_MISSING 1
@@ -155,14 +145,8 @@
 /* Define to `__inline__' or `__inline' if that's what the C compiler
    calls it, or to nothing if 'inline' is not supported under any name.  */
 #ifndef __cplusplus
-# ifndef inline
-#  define inline __inline
-# endif
+/* #undef inline */
 #endif
-
-/* Windows doesn't have popen, but it does have _popen. */
-#define popen _popen
-#define pclose _pclose
 
 #include <stddef.h>
 
@@ -192,9 +176,7 @@ char *strerror(int errno);
 # include <fcntl.h>
 # include <io.h>
 # define isatty _isatty
-# if defined(_MSC_VER) && _MSC_VER < 1900
-#  define snprintf _snprintf
-# endif
+# define snprintf _snprintf
 #endif
 
 #endif /* GIFSICLE_CONFIG_H */
